d1e8dc4dd75b4e1148b88be4af57a03a152ba637
Flawedspirit <me@flawedspirit.ca>
2022-01-17 22:18:50 -0500

    Patch version bump

--- PayoutTester/Properties/AssemblyInfo.cs
+++ PayoutTester/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PayoutTester")]
-[assembly: AssemblyCopyright("Copyright Marc Chiarelli © 2018")]
+[assembly: AssemblyCopyright("Marc Chiarelli © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: AssemblyFileVersion("1.2.2")]
